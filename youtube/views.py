c95549e1442724a572e5293a53327c8841da186c
shrutiverma2107 <shrutiverma2107@gmail.com>
2020-11-06 18:40:07 +0530

    Code cleaned

--- youtube/views.py
+++ youtube/views.py
@@ -4,11 +4,10 @@ from django.http import HttpResponse
 from django.views.generic import ListView
 
 def index(request):
-    template_name = 'video.html'
     vd = Videos.objects.all()
     if request.GET.get('search'):
         query = request.GET.get('search')
         vd = Videos.objects.filter(name__icontains=query)
     else :
         vd=Videos.objects.all() # Collect all records from table 
-    return render(request,'video_library_single.html',{'videos':vd})
+    return render(request,'video_library.html',{'videos':vd})
